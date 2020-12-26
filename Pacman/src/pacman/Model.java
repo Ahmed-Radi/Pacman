305ff77ac63b920f2d7cd5cab3d3e01b5914e3c7
Ahmed Radi <ahmedradi743@gmail.com>
2020-12-26 10:18:32 +0200

    Add image and display it

--- Pacman/src/pacman/Model.java
+++ Pacman/src/pacman/Model.java
@@ -5,8 +5,29 @@ package pacman;
  *
  * @author Ahmed
  */
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 public class Model {
-    
+    private Dimension d;
+    private final Font smallFont = new Font("Arial", Font.BOLD, 14);
+    private boolean inGame = false;
+    private boolean dying = false;
+
+    private final int BLOCK_SIZE = 24;
+    private final int N_BLOCKS = 15;
+    private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
+
+
     /*
         0 = blue 
         1 = left border 
@@ -32,12 +53,20 @@ public class Model {
         17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
     };
-    
-        public Model() {
+    private void loadImages() {
+        down  = new ImageIcon("src\\images/down.gif").getImage();
+        up    = new ImageIcon("src\\images/up.gif").getImage();
+        left  = new ImageIcon("src\\images/left.gif").getImage();
+        right = new ImageIcon("src\\images/right.gif").getImage();
+        ghost = new ImageIcon("src\\images/ghost.gif").getImage();
+        heart = new ImageIcon("src\\images/heart.png").getImage();
+    }
+    public Model() {
         loadImages();
         initVariables();
         addKeyListener(new TAdapter());
         setFocusable(true);
         initGame();
     }
+    
 }
