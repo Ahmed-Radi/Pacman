90f197692f2a7552777369963fe56ff4eb6f6bb9
sarah1150 <58861704+sarah1150@users.noreply.github.com>
2021-01-03 23:09:47 +0200

    Class TAdapter
    Class TAdapter that control moving ....

--- Pacman/src/pacman/Model.java
+++ Pacman/src/pacman/Model.java
@@ -402,6 +402,38 @@ public class Model {
         g2d.dispose();
     }
 
+
+  class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    req_dx = -1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    req_dx = 1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    req_dx = 0;
+                    req_dy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    req_dx = 0;
+                    req_dy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                } 
+            } else {
+                if (key == KeyEvent.VK_SPACE) {
+                    inGame = true; 
+                    initGame();// Press Space to Start Game
+                }
+            }
+        }
+}
     
     
 }
